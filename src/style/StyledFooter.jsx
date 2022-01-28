b9f1f74bf04fbae9000c279e55e5743c25b6397d
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 14:28:02 -0500

    h

---
+++ src/style/StyledFooter.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+    background: #232E35;
+    width: 100%;
+    display: flex;
+`
+export const Item = styled.div`
+    width: 25%;
+    display: inline-block;
+`
+export const ImgenIcon = styled.div`
+    width: 90%;
+    margin: auto;
+`
