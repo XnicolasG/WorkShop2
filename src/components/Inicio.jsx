54e1baee533bfe0bb31e6e528ea7702c4d785ad2
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 16:06:37 -0500

    botones

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -8,9 +8,18 @@ const Inicio = () => {
   return (
   <ContInit>
       <Mensaje>Practica tus conocimientos en la categoría que prefieras.</Mensaje>
+      
       <BotonHtml>
-<Link to = "" onClick = {toPage('')}><img src="" alt="" /></Link>
+<Link to = "#" onClick = {toPage()}><img src="https://res.cloudinary.com/dlkynkfvq/image/upload/v1643380129/workshop-2/HTMLLOGO_rjxhd3.png" alt="HTML" /></Link>
       </BotonHtml>
+
+      <BotonJs>
+      <Link to = "#" onClick = {toPage()}><img src="https://res.cloudinary.com/dlkynkfvq/image/upload/v1643380340/workshop-2/JsLogo_b5fjti.png" alt="JavaScript." /></Link>
+      </BotonJs>
+
+<BotonCSS>
+<Link to = "#" onClick = {toPage()}><img src="https://res.cloudinary.com/dlkynkfvq/image/upload/v1643380233/workshop-2/CssLogo_vpj0p1.png" alt="CSS" /></Link>
+</BotonCSS>
     
   </ContInit>
   );
