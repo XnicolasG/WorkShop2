81de7884f07710ec5879bb3ec802e807a41c2a61
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 14:21:26 -0500

    5

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,4 +1,8 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FooterCont from '../Components/footer';
+import { Home } from '../Components/Home';
+import { Login } from '../Components/Login';
+import { Logo } from '../Styles/HomeStyles';
 import Container from '../container/Container';
 function App() {
   return (
