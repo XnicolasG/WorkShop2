3572e961d8b59b4ebe39395348886b11a11c8715
Nicolás García <nicolasg.ngl@gmail.com>
2022-01-28 14:13:37 -0500

    agregrando estilos container

--- src/style/StyledCont.jsx
+++ src/style/StyledCont.jsx
@@ -4,4 +4,32 @@ export const ContIni = styled.div`
     background: black;
     width: 100%;
     height: 100vh;
+    padding: 5%;
+`
+export const ContImg = styled.div`
+    width: 45%;
+    margin: auto;
+`
+export const Logo = styled.img` 
+    width: 30%;
+`
+export const Titulo = styled.h1`
+    color: white;
+    font-size: 2.5em;
+    text-align: center;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+`
+export const Formato = styled.div`
+    width: 60%;
+    margin: auto;
+`
+export const Inputs = styled.input`
+    display: block;
+    width: 50%;
+    padding: 2%;
+    font-size: 1.1rem;
+    margin: auto;
+    border: 2px solid white;
+    border-radius: 5px;
+    margin-bottom: 2%;
 `
